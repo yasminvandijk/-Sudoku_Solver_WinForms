828d16174b5edaeedd2d0cee434b179a52298846
Yasmin van Dijk <vandijk.yasmin@gmail.com>
2020-08-20 20:25:36 +0200

    added sudoku input form

---
+++ Sudoku_Solver/Sudoku_Solver/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Sudoku_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel_sudoku = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 600F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel_sudoku, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 600F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(723, 645);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel_sudoku
+            // 
+            this.tableLayoutPanel_sudoku.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel_sudoku.ColumnCount = 3;
+            this.tableLayoutPanel_sudoku.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel_sudoku.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel_sudoku.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel_sudoku.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_sudoku.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel_sudoku.Name = "tableLayoutPanel_sudoku";
+            this.tableLayoutPanel_sudoku.RowCount = 3;
+            this.tableLayoutPanel_sudoku.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel_sudoku.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel_sudoku.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel_sudoku.Size = new System.Drawing.Size(594, 594);
+            this.tableLayoutPanel_sudoku.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 689);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Sudoku Solver";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_sudoku;
+    }
+}
+
---
+++ Sudoku_Solver/Sudoku_Solver/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku_Solver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            AddSudokuCells();
+        }
+
+        private void AddSudokuCells()
+        {
+            // add 9 blocks with 3 by 3 cells
+            for (int blockNr = 0; blockNr < 9; blockNr++)
+            {
+                // create a table layout panel with 3 rows and 3 columns
+                TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+                tableLayoutPanel.Dock = DockStyle.Fill;
+
+                tableLayoutPanel.RowCount = 3;
+                tableLayoutPanel.ColumnCount = 3;
+                tableLayoutPanel.RowStyles.Clear();
+                tableLayoutPanel.ColumnStyles.Clear();
+
+                for (int i = 0; i < 3; i++)
+                {
+                    tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 33));
+                    tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33));
+                }
+
+                // add 9 rich text boxes to the table layout panel
+                for (int i = 0; i < 9; i++)
+                {
+                    RichTextBox richTextBox = new RichTextBox();
+                    richTextBox.Dock = DockStyle.Fill;
+                    richTextBox.Font = new Font(FontFamily.GenericSansSerif, 24);
+                    richTextBox.BorderStyle = BorderStyle.None;
+
+                    tableLayoutPanel.Controls.Add(richTextBox);
+
+                    // compute row and column index and add callback function
+                    int row = 3 * (int)Math.Floor(blockNr / 3.0) + (int)Math.Floor(i / 3.0);
+                    int column = 3 * (blockNr % 3) + (i % 3);
+
+                    richTextBox.TextChanged += (sender, e) => UpdateCellValue(richTextBox, row, column);
+                }
+
+                tableLayoutPanel_sudoku.Controls.Add(tableLayoutPanel);
+            }
+        }
+
+        private void UpdateCellValue(RichTextBox richTextBox, int row, int column)
+        {
+            //
+            MessageBox.Show($"{richTextBox.Text} {row} {column}");
+        }
+    }
+}
